8100480e301ad2d1879578abe3a5db38cc5762b8
Abdullah Mumuni <61237306+abdullah-byte@users.noreply.github.com>
2023-12-06 02:11:22 +0100

    Delete libraryCat directory

--- libraryCat/library/src/main/java/com/olu/library/repo/BookRepo.java
+++
@@ -1,7 +0,0 @@
-package com.olu.library.repo;
-
-import com.olu.library.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepo extends JpaRepository<Book, Long> {
-}
